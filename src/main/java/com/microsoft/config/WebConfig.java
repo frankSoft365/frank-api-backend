97afab9a327d7d2f64a5040747c8abb43ed62c3c
frank <264430662@qq.com>
2026-06-30 23:06:33 +0800

    trigger rebuild

--- src/main/java/com/microsoft/config/WebConfig.java
+++ src/main/java/com/microsoft/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private TokenInterceptor tokenInterceptor;
 
+    // 配置跨域请求
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
